f1f00cb963634df44dfc436a7681d858b2ed2ae6
Aolslekej <alexander_baranovsky@mail.ru>
2026-01-16 16:51:56 +0300

    blocks

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./style/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="block">
+        <h1>Hello</h1>
+        <p>Lorem*15</p>
+      </div>
+      <div className="block">
+        <h1>Hello2</h1>
+        <p>Lorem*15</p>
+      </div>
+      <div className="block">
+        <h1>Hello3</h1>
+        <p>Lorem*15</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
